d8baddd67c8d86b7eadab4918412479ef9b770d0
Rub21 <rub2106@gmail.com>
2019-11-04 15:58:20 -0500

    Add function to process many areas(bboxes)

--- start.sh
+++ start.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
-label-maker download
-label-maker labels
-label-maker images
-label-maker package
+# In case there are more than one bbox to evaluate.
+bboxesFile=bboxes.txt
+if [ -f $bboxesFile ]; then
+   index=1
+   while IFS= read -r bbox 
+   do
+        dest=$(cat config.json | jq '.country')
+        dest="${dest//\"}"
+        configFile="${dest}_area${index}_config.json"
+        mkdir $dest
+        cat config.json | jq '.bounding_box='$bbox > $dest/$configFile
+        echo "Processing $configFile ..."
+        if [ ! -f "$dest/$dest.mbtiles" ]; then
+            label-maker download -d $dest -c $dest/$configFile
+        fi
+        label-maker labels -d $dest -c $dest/$configFile
+        label-maker images -d $dest -c $dest/$configFile
+        label-maker package -d $dest -c $dest/$configFile
+        mv $dest/labels.npz $dest/${dest}_area${index}_labels.npz
+        mv $dest/data.npz $dest/${dest}_area${index}_data.npz
+        mv $dest/classification.geojson $dest/${dest}_area${index}_classification.geojson
+        index=$((index+1))
+    done < $bboxesFile
+else
+    label-maker download
+    label-maker labels
+    label-maker images
+    label-maker package
+fi
+
+
+
+
