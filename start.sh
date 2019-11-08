c56fbaac0f036b65a1a81cc9fcdb1092fe5a1390
Rub21 <rub2106@gmail.com>
2019-11-08 14:19:38 -0500

    Fix label download

--- start.sh
+++ start.sh
@@ -6,21 +6,23 @@ if [ -f $bboxesFile ]; then
    do
         dest=$(cat config.json | jq '.country')
         dest="${dest//\"}"
-        configFile="${dest}_area${index}_config.json"
+        country=$dest
+        configFile="${country}_area${index}_config.json"
         if [ ! -d "$dest" ]; then
             mkdir $dest
         fi
         cat config.json | jq '.bounding_box='$bbox > $dest/$configFile
-        echo "Processing $configFile ..."
-        if [ ! -f "$dest/$dest.mbtiles" ]; then
-            label-maker download -d $dest -c $dest/$configFile
-        fi
+        echo "Processing ..........................................: $configFile"
+        label-maker download -d $dest -c $dest/$configFile
         label-maker labels -d $dest -c $dest/$configFile
         label-maker images -d $dest -c $dest/$configFile
         label-maker package -d $dest -c $dest/$configFile
-        mv $dest/labels.npz $dest/${dest}_area${index}_labels.npz
-        mv $dest/data.npz $dest/${dest}_area${index}_data.npz
-        mv $dest/classification.geojson $dest/${dest}_area${index}_classification.geojson
+        mv $dest/labels.npz $dest/${country}_area${index}_labels.npz
+        mv $dest/data.npz $dest/${country}_area${index}_data.npz
+        mv $dest/classification.geojson $dest/${country}_area${index}_classification.geojson
+        rm $dest/${country}.geojson
+        rm $dest/${country}.mbtiles
+        rm $dest/${country}-z18.mbtiles
         index=$((index+1))
     done < $bboxesFile
 else
