0cd4bc5077b4113b57091c48f689ce4dd60f2943
Rub21 <rub2106@gmail.com>
2019-11-13 15:44:31 -0500

    Update the processing way

--- start.sh
+++ start.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+# Config file and dest directory
+dest=$(cat config.json | jq '.country')
+dest="${dest//\"}"
+configFile="config.json"
+if [ ! -d "$dest" ]; then
+    mkdir $dest
+    cp $configFile $dest/$configFile
+fi
+
 bboxesFile=bboxes.txt
 if [ -f $bboxesFile ]; then
    index=1
    while IFS= read -r bbox 
    do
-        dest=$(cat config.json | jq '.country')
-        dest="${dest//\"}"
         country=$dest
         configFile="${country}_area${index}_config.json"
-        if [ ! -d "$dest" ]; then
-            mkdir $dest
-        fi
-        cat config.json | jq '.bounding_box='$bbox > $dest/$configFile
+        cat $dest/config.json | jq '.bounding_box='$bbox > $dest/$configFile
         echo "Processing ..........................................: $configFile"
         label-maker download -d $dest -c $dest/$configFile
         label-maker labels -d $dest -c $dest/$configFile
@@ -26,10 +30,10 @@ if [ -f $bboxesFile ]; then
         index=$((index+1))
     done < $bboxesFile
 else
-    label-maker download
-    label-maker labels
-    label-maker images
-    label-maker package
+    label-maker download -d $dest -c $dest/$configFile
+    label-maker labels -d $dest -c $dest/$configFile
+    label-maker images -d $dest -c $dest/$configFile
+    label-maker package -d $dest -c $dest/$configFile
 fi
 
 
