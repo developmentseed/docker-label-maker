e252daf3507bda1326658b1d258630e8f28039d0
Rub21 <rub2106@gmail.com>
2019-11-13 15:56:34 -0500

    Set config file

--- start.sh
+++ start.sh
@@ -7,34 +7,7 @@ if [ ! -d "$dest" ]; then
     mkdir $dest
 fi
 cp $configFile $dest/
-bboxesFile=bboxes.txt
-if [ -f $bboxesFile ]; then
-   index=1
-   while IFS= read -r bbox 
-   do
-        country=$dest
-        configFile="${country}_area${index}_config.json"
-        cat $dest/config.json | jq '.bounding_box='$bbox > $dest/$configFile
-        echo "Processing ..........................................: $configFile"
-        label-maker download -d $dest -c $dest/$configFile
-        label-maker labels -d $dest -c $dest/$configFile
-        label-maker images -d $dest -c $dest/$configFile
-        label-maker package -d $dest -c $dest/$configFile
-        mv $dest/labels.npz $dest/${country}_area${index}_labels.npz
-        mv $dest/data.npz $dest/${country}_area${index}_data.npz
-        mv $dest/classification.geojson $dest/${country}_area${index}_classification.geojson
-        rm $dest/${country}.geojson
-        rm $dest/${country}.mbtiles
-        rm $dest/${country}-z18.mbtiles
-        index=$((index+1))
-    done < $bboxesFile
-else
-    label-maker download -d $dest -c $dest/$configFile
-    label-maker labels -d $dest -c $dest/$configFile
-    label-maker images -d $dest -c $dest/$configFile
-    label-maker package -d $dest -c $dest/$configFile
-fi
-
-
-
-
+label-maker download -d $dest -c $dest/$configFile
+label-maker labels -d $dest -c $dest/$configFile
+label-maker images -d $dest -c $dest/$configFile
+label-maker package -d $dest -c $dest/$configFile
