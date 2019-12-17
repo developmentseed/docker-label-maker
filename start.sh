b19109f5dc101dc879ddbe73b198d7f28d20df8d
Rub21 <rub2106@gmail.com>
2019-12-17 12:31:10 -0500

    Updates on config

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Config file and dest directory
 dest=$(cat config.json | jq '.country')
-dest="${dest//\"}_background"
+dest="${dest//\"}"
 configFile="config.json"
 if [ ! -d "$dest" ]; then
     mkdir $dest
 fi
 cp $configFile $dest/
-# label-maker download -d $dest -c $dest/$configFile
-# label-maker labels -d $dest -c $dest/$configFile
+echo "Running... $dest"
+label-maker download -d $dest -c $dest/$configFile
+label-maker labels -d $dest -c $dest/$configFile
 label-maker images -d $dest -c $dest/$configFile
 label-maker package -d $dest -c $dest/$configFile
