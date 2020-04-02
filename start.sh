e28bb21dfee31c7d52b5797b3986628b0e0b98cc
Rub21 <rub2106@gmail.com>
2020-04-02 13:33:00 -0500

    Update bash

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Config file and dest directory
 dest=$(cat config.json | jq '.country')
+if [ "$dest" = "null" ]; then
+    dest="pakistan"
+fi
 dest="${dest//\"}"
 configFile="config.json"
-if [ ! -d "$dest" ]; then
-    mkdir $dest
-fi
 cp $configFile $dest/
 echo "Running... $dest"
-label-maker download -d $dest -c $dest/$configFile
-label-maker labels -d $dest -c $dest/$configFile
+# label-maker download -d $dest -c $dest/$configFile
+label-maker labels -s -d $dest -c $dest/$configFile
 label-maker images -d $dest -c $dest/$configFile
-label-maker package -d $dest -c $dest/$configFile
+# label-maker package -d $dest -c $dest/$configFile
