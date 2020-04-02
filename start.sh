0c4d5a3054f247313152908f0aa5aeba1c45f3bc
Rub21 <rub2106@gmail.com>
2020-04-02 13:36:43 -0500

    Update

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 # Config file and dest directory
 dest=$(cat config.json | jq '.country')
 if [ "$dest" = "null" ]; then
-    dest="pakistan"
+    dest="data"
 fi
 dest="${dest//\"}"
-configFile="config.json"
-cp $configFile $dest/
+mkdir -p $dest
+cp config.json $dest/
 echo "Running... $dest"
-# label-maker download -d $dest -c $dest/$configFile
-label-maker labels -s -d $dest -c $dest/$configFile
-label-maker images -d $dest -c $dest/$configFile
-# label-maker package -d $dest -c $dest/$configFile
+# label-maker download -d $dest -c $dest/config.json
+label-maker labels -s -d $dest -c $dest/config.json
+label-maker images -d $dest -c $dest/config.json
+# label-maker package -d $dest -c $dest/config.json
